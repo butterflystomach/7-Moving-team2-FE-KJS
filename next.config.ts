2effdb28efe6d08ae18ecb4f8a169a32b11f9bea
KimJesung <andrewkim99@naver.com>
2025-12-15 14:29:02 +0900

    chore: 세팅 점검 및 reactStrictMode 활성화

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // 외부 이미지로 기본 프로필 이외의 이미지 출력 테스트용
+  reactStrictMode: true,
   images: {
     domains: ["codeit-images.codeit.com"],
   },
